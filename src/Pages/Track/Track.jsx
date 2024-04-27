c0e9bfa8b0875c6b45526e7d3f3c614ad641685e
wendel <wendellduos@hotmail.com>
2024-04-27 08:46:06 -0300

    feat: added leaflet

--- src/Pages/Track/Track.jsx
+++ src/Pages/Track/Track.jsx
@@ -1,6 +1,10 @@
 import "./Track.css";
 import Header from "../../Components/Header/Header";
-
+import {
+  MapContainer,
+  TileLayer,
+  useMap,
+} from "https://cdn.esm.sh/react-leaflet";
 const Track = () => {
   return (
     <>
