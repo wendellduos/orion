3d2f9cf5024e1a12615b2263e731db205b75e808
wendel <wendellduos@hotmail.com>
2024-04-27 08:40:35 -0300

    feat: created Broadcast

--- src/Pages/Broadcast/Broadcast.jsx
+++ src/Pages/Broadcast/Broadcast.jsx
@@ -1,10 +1,25 @@
 import "./Broadcast.css";
 import Header from "../../Components/Header/Header";
+import { useState } from "react";
 
 const Broadcast = () => {
+  const statusList = ["idle", "sharing", "error"];
+
+  const [shareStatus, setShareStatus] = useState(statusList[0]);
+
+  const handleButton = () => {
+    // very temporary setting
+    setShareStatus(statusList[Math.floor(Math.random() * 3)]);
+  };
+
   return (
     <>
       <Header currentPage={"broadcast"} />
+      <main className="Broadcast-main">
+        <button type="button" className={shareStatus} onClick={handleButton}>
+          {shareStatus}
+        </button>
+      </main>
     </>
   );
 };
