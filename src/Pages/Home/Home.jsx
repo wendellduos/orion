85f207795a2a59cbf3391b4fd08d1876651b48fd
wendel <wendellduos@hotmail.com>
2024-04-28 22:39:20 -0300

    feat: added map into Home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,28 @@
 import "./Home.css";
 import Header from "../../Components/Header/Header";
 import DecoratedBlock from "../../Components/DecoratedBlock/DecoratedBlock";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { useState } from "react";
 
 const Home = () => {
-  const speedHistory = [0, 39, 31, 100, 63, 57];
+  const [mapState, setMapState] = useState(null);
+  const [speedHistory, setSpeedHistory] = useState([]);
+  const [latestCoords, setLatestCoords] = useState({
+    lat: -26.301581,
+    lng: -48.846323,
+  });
+
+  const handleInsertSpeedPoint = (speed) => {
+    let newArray;
+
+    if (speedHistory.length < 6) {
+      newArray = [...speedHistory, speed];
+    } else {
+      newArray = [...speedHistory.slice(1), speed];
+    }
+
+    setSpeedHistory(newArray);
+  };
 
   return (
     <>
@@ -24,7 +43,7 @@ const Home = () => {
                 className="speed-point"
                 key={index}
                 style={{
-                  left: `${index * 19.7}%`,
+                  left: `${index * 19.9}%`,
                   height: `${speed}%`,
                 }}
               >
@@ -37,6 +56,37 @@ const Home = () => {
             );
           })}
         </div>
+        <br />
+        <br />
+        <h3 className="mini-title">Última localização</h3>
+        <div className="preview-map-wrp">
+          <MapContainer
+            center={[latestCoords.lat, latestCoords.lng]}
+            zoom={16}
+            ref={mapState}
+            touchZoom={false}
+            scrollWheelZoom={false}
+            zoomControl={false}
+            dragging={false}
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            <TileLayer
+              attribution='<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy;JawgMaps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://tile.jawg.io/jawg-lagoon/{z}/{x}/{y}{r}.png?access-token=X0Pc7SpgNT4B4xu7nQMaXHYusv0CrKZSwG0KBCJH7zNkg6rz4i30t4dzoNMa4Ajd"
+            />
+          </MapContainer>
+        </div>
+        <a
+          className="open-on-gmaps"
+          href={`https://www.google.com.br/maps/@${latestCoords.lat},${latestCoords.lng},16z?entry=ttu`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ver localização no Google Maps
+        </a>
       </DecoratedBlock>
     </>
   );
