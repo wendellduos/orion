4ebad23c0ec7e83550d00dc760bafa174912d245
wendel <wendellduos@hotmail.com>
2024-04-29 00:02:46 -0300

    feat: initiated Login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,25 @@
 import "./Login.css";
+import DecoratedBlock from "../../Components/DecoratedBlock/DecoratedBlock";
 
 const Login = () => {
   return (
-    <>
-      <p>This is the login page</p>
-    </>
+    <DecoratedBlock>
+      <h1 className="login-h1">
+        <img
+          className="login-logo"
+          src="/assets/img/logo.png"
+          alt="logo orion"
+        />
+      </h1>
+      <h2 className="login-txt">login</h2>
+      <form className="login-form">
+        <input type="text" placeholder="Usuário" />
+        <input type="password" placeholder="Senha" />
+        <button className="login-btn" onClick={(e) => e.preventDefault()}>
+          Entrar
+        </button>
+      </form>
+    </DecoratedBlock>
   );
 };
 
